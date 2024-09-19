a5dce35d114b1398600b0c64a5186062e453d439
anarha <haqverdiyevanar@outlook.com>
2024-09-19 18:58:25 +0400

    Install php-zip

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,11 +8,10 @@ set -xe
 apt-get update \
 && apt-get install -y \
 && apt-get autoremove -y \
-&& docker-php-ext-install mysqli pdo pdo_mysql\
+&& docker-php-ext-install mysqli pdo pdo_mysql zip\
 && apt-get install curl -y \
 && apt-get install git -y\
 && apt-get install libzip-dev zip -y\
-&& apt-get install php8.3-zip -y\
 && curl -sS https://get.symfony.com/cli/installer | bash \
 && curl -sS https://getcomposer.org/installer | php
 #!/bin/bash
